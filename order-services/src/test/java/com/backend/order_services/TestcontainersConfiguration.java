36a93571b81636ba6c3960e44bc844732ca6ced4
satya_santosh <seethepallisantosh@gmail.com>
2025-10-25 22:27:27 +0530

    [Created] folders in microservices style including required dependencies

---
+++ order-services/src/test/java/com/backend/order_services/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.backend.order_services;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
---
+++ user-service/src/test/java/com/backend/user_management_service/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.user_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
