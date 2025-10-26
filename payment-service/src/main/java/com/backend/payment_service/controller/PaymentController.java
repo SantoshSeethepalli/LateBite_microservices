24a18557b7688f7af832018d4bc68d05a6b7d1d4
satya_santosh <seethepallisantosh@gmail.com>
2025-10-26 20:58:03 +0530

    [Done] Payment Service

--- payment-service/src/main/java/com/backend/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/backend/payment_service/controller/PaymentController.java
@@ -2,14 +2,13 @@ package com.backend.payment_service.controller;
 
 import com.backend.payment_service.service.PaymentService;
 import com.google.zxing.WriterException;
+import jdk.jfr.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -17,14 +16,18 @@ import java.math.BigDecimal;
 
 
 @RestController
-@RequestMapping("/api/payment")
+@RequestMapping(value = "/api/payment", produces = MediaType.IMAGE_PNG_VALUE)
+@ResponseStatus(HttpStatus.OK)
 public class PaymentController {
 
     @Autowired
     PaymentService paymentService;
 
     @GetMapping("/generate-qr")
-    public byte[] getQrByUpiIdAndAmount(@RequestParam String restaurantUpiId, @RequestParam BigDecimal amount) throws IOException, WriterException {
+    public byte[] getQrByUpiIdAndAmount(
+                @RequestParam String restaurantUpiId,
+                @RequestParam BigDecimal amount)
+            throws IOException, WriterException {
 
         return paymentService.generateQr(restaurantUpiId, amount);
     }
