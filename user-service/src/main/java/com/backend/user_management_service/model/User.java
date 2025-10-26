59b68125a687f14c34c56f52699a888552af6d45
satya_santosh <seethepallisantosh@gmail.com>
2025-10-27 03:41:30 +0530

    [Created] User entity

---
+++ user-service/src/main/java/com/backend/user_management_service/model/User.java
@@ -0,0 +1,36 @@
+package com.backend.user_management_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "t_user")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
