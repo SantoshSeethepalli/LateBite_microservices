2508c755e66986436267af11b17ad9cafe800eb4
satya_santosh <seethepallisantosh@gmail.com>
2025-10-26 17:29:45 +0530

    [Added Dependency] lombok

--- order-service/src/main/java/com/backend/order_services/model/Order.java
+++ order-service/src/main/java/com/backend/order_services/model/Order.java
@@ -1,7 +1,15 @@
 package com.backend.order_services.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
 
 @Data
+@Builder
 public class Order {
 
+    @Id
+    @GeneratedValue(GeneratedValue)
+    private Long id;
 }
