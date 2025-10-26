a58f8b51bb1a3643b323298217c4ca5a70fba5ec
satya_santosh <seethepallisantosh@gmail.com>
2025-10-26 17:47:50 +0530

    [Completed] Order entity

--- order-service/src/main/java/com/backend/order_services/model/Order.java
+++ order-service/src/main/java/com/backend/order_services/model/Order.java
@@ -1,15 +1,43 @@
 package com.backend.order_services.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Data;
+import com.backend.order_services.model.Enums.OrderStatus;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     @Id
-    @GeneratedValue(GeneratedValue)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long restaurantId;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus orderStatus;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
