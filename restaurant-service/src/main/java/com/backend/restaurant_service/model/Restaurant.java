834dfbbc6b1af639ea835882e30c009766cabf71
satya_santosh <seethepallisantosh@gmail.com>
2025-10-29 16:50:01 +0530

    [Created] Restaurant & MenuItem entities

---
+++ restaurant-service/src/main/java/com/backend/restaurant_service/model/Restaurant.java
@@ -0,0 +1,40 @@
+package com.backend.restaurant_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "t_restaurant")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String upiId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
