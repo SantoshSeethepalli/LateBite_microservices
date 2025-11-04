e24507f12cd7de7d1b149d175aaa278408ed8942
satya_santosh <seethepallisantosh@gmail.com>
2025-11-04 19:45:14 +0530

    [Deleted] sample code in OrderServicesApplication

--- order-service/src/main/java/com/backend/order_services/OrderServicesApplication.java
+++ order-service/src/main/java/com/backend/order_services/OrderServicesApplication.java
@@ -6,17 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class OrderServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServicesApplication.class, args);
 	}
-
-	@GetMapping("/sayHello")
-	public String sayHello() {
-
-		return "Hello World";
-	}
-
 }
