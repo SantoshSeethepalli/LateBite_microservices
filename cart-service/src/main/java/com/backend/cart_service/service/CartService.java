985aedec50aab5b5891755122eda82b063430f20
Ritwik Karthikeya K <125467946+ritwikkarthikeya@users.noreply.github.com>
2025-11-09 19:18:15 +0530

    [Updated] OrderStatus-Enum Updated

--- cart-service/src/main/java/com/backend/cart_service/service/CartService.java
+++ cart-service/src/main/java/com/backend/cart_service/service/CartService.java
@@ -16,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -34,6 +35,8 @@ public class CartService {
                             .userId(userId)
                             .restaurantId(restaurantId)
                             .totalAmount(BigDecimal.ZERO)
+                            .createdAt(LocalDateTime.now())
+                            .updatedAt(LocalDateTime.now())
                             .build();
                     return cartRepository.save(newCart);
                 });
